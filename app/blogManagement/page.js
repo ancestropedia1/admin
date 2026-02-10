92f0f31e4d0b35497a9600770ec10b0eae4b64cc
ancestropedia <ancestropedia1@gmail.com>
2026-02-10 18:56:53 +0530

    change

--- app/blogManagement/page.js
+++ app/blogManagement/page.js
@@ -1,6 +1,6 @@
 "use client";
-import BlogManagement from "@/component/blogmanagement/BlogManagement.js";
-import protectPage from "../utils/protectPage";
+import BlogManagement from "../../component/blogmanagement/BlogManagement.js";
+import protectPage from "../utils/protectPage.js";
 
 function Page() {
   return <div><BlogManagement/></div>;
