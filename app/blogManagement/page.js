53dfad8079c3e50095e0a530e10af2f9376e9659
ancestropedia <ancestropedia1@gmail.com>
2026-02-11 13:20:45 +0530

    tst

--- app/blogManagement/page.js
+++ app/blogManagement/page.js
@@ -1,10 +1,11 @@
 "use client";
 import BlogManagement from "../../component/blogmanagement/BlogManagement.js";
-import protectPage from "../utils/protectPage.js";
+// import protectPage from "../utils/protectPage.js";
 
-function Page() {
+function page() {
   return <div><BlogManagement/></div>;
 }
 
 // 🚨 ONLY users with "blogmanagement" permission can open this page
-export default protectPage("blogmanagement")(Page);
+// export default protectPage("blogmanagement")(page);
+export default page;
