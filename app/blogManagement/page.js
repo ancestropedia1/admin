e3e55f74f1a2fad879bbe6aaaa89a694b141c15c
ancestro 2 <ancestropedia2@gmail.com>
2026-05-06 16:21:29 +0530

    blogs

--- app/blogManagement/page.js
+++ app/blogManagement/page.js
@@ -1,5 +1,5 @@
 "use client";
-import { BlogManagement } from "@/component/blogmanagement/BlogManagement";
+import  BlogManagement  from "@/component/blogmanagement/BlogManagement";
 // import protectPage from "../utils/protectPage.js";
 
 function page() {
